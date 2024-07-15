91b40bb55726ae2c549898c8f1f1aa373d0d28a1
quorafind <951011105@qq.com>
2024-07-15 22:24:45 +0800

    chore: update readme

--- main.ts
+++ main.ts
@@ -1,9 +1,56 @@
+import { EditorState } from '@codemirror/state';
 import { Plugin } from 'obsidian';
 
-export default class MyPlugin extends Plugin {
+const addQuoteOnPaste = (plugin: PasteMasterPlugin) => {
+	return EditorState.transactionFilter.of((tr) => {
+		if (!tr.docChanged) {
+			return tr;
+		}
+
+		if (!plugin.isPasting) return tr;
+		const changes: { from: number; to: number; insert: string; }[] = [];
+
+		tr.changes.iterChanges((fromA, toA, fromB, toB, inserted) => {
+			if (inserted.lines <= 1) return;
+
+			if (inserted.sliceString(0, inserted.length).contains('\n>')) return;
+
+			const line = tr.startState.doc.lineAt(fromA);
+			if (!line.text.startsWith('>')) return;
+
+			const text = inserted.sliceString(0, inserted.length);
+			const quotedText = text.split('\n').map((line, index) => {
+				if (index === 0) return line;
+				return '> ' + line;
+			}).join('\n');
+			changes.push({
+				from: fromA,
+				to: toA,
+				insert: quotedText
+			});
+		});
+
+		if (!changes.length) return tr;
+
+		plugin.isPasting = false;
+		return [
+			{
+				changes,
+				userEvent: "input.paste.addQuote",
+			}
+		];
+	});
+};
+
+
+export default class PasteMasterPlugin extends Plugin {
+	isPasting = false;
 
 	async onload() {
-
+		this.app.workspace.on('editor-paste', () => {
+			this.isPasting = true;
+		});
+		this.registerEditorExtension([addQuoteOnPaste(this)]);
 	}
 
 	onunload() {
